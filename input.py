b4b743eac867024c72f8bbaafcab779359b62c2a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-04-27 14:37:40 +0200

    Remove unused imports

--- input.py
+++ input.py
@@ -1,7 +1,6 @@
-import threading
 import numpy as np
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, ClientSettings
+from streamlit_webrtc import webrtc_streamer
 from PIL import Image
 import cv2
 import imutils
